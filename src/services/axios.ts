9740e5f0ca0a8090b49f1bcbab5aece5e922c48f
Abanoub George <abanoub.george@kemitt.com>
2025-04-23 12:36:42 +0200

    remove commented-out authorization headers from axios configuration

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 const globalAxios = axios.create({
     baseURL: 'https://wedding-img-backend-production.up.railway.app/',
-    // headers: {
-    //   Authorization: hasToken ? `Bearer ${hasToken.token}` : '',
-    //   "auth-token": hasToken ? `${hasToken.token}` : ''
-    // },
+
+
 });
 
 export default globalAxios;

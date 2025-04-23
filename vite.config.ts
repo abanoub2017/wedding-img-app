6db09e0eb1f6370c97451632b845822c386bbe37
Abanoub George <abanoub.george@kemitt.com>
2025-04-23 12:04:52 +0200

    add GitHub Pages deployment workflow and update package.json for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,25 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//     tailwindcss(),
+
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,14 +27,19 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+// Get the repo name from package.json or use environment variable
+// This will be used as the base path for GitHub Pages
+const baseUrl = process.env.GITHUB_REPOSITORY
+  ? `/${'wedding-img-app'.split('/')[1]}/`
+  : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: baseUrl, // Set the base URL for GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
-
   ],
   resolve: {
     alias: {
